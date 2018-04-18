6be54dcdf83853e03d72c98dcfdaa56471d310ea
Erik Erlandson <eerlands@redhat.com>
2018-04-18 16:42:56 -0700

    vector B, B3 basis

--- src/main/scala/com/manyangled/snowball/infra.scala
+++ src/main/scala/com/manyangled/snowball/infra.scala
@@ -27,21 +27,23 @@ package com.manyangled.snowball
  */
 object infra {
   // conventions
-  // m: the number of piecewise intervals between knots
-  // (t0, tm): min and maximum of the interpolation range
-  // alpha: (tm - t0) / m, the spacing between knot points
-  // (t0, t1, ... tm): the knot points (equally spaced by alpha)
   // n: the number of data points to interpolate (N in the paper)
   // (u1, d1), (u2, d2)... (un, dn): data points to interpolate
-  // mm = m + 3 (M in the paper); 3 is from degree-3 (cubic) B-spline
-  // lambda: smoothing weight parameter
-  // w1, w2, ... wn: interpolation weights for data (larger -> fit closer)
+  // w1, w2, ... wn: interpolation weights for data; larger --> fit closer
+  // umin, umax: bounds of the interpolation range; e.g. could be (u1,un)
+  // m = number of knot intervals covering interpolation interval [umin, umax]
+  // mm = the number of knot intervals (M in the paper); mm = m + 3
+  // In the paper, knot points ti are indexed from -3, -2, ... m-1
+  // knot points t(-3), t(-2), t(-1) lie to the left of umin
+  // t(0) = umin.  knots t(0) ... t(m-1) lie on the interval [umin, umax]
+  // alpha: 1/alpha is the spacing between knot points; alpha*(t(j+1) - t(j)) = 1; see Eq(2)
+  // alpha = m / (umax - umin)
+  // lambda: smoothing weight parameter; larger --> smoother, i.e. lower curvature
+  // tau: vector of control points.  tau(i) is ith control point.
 
-  // returns matrix Rinf (26) for size MxM, where M = m+3
-  def matrixRinf(m: Int): Array[Array[Double]] = {
-    val mm = m + 3
-    // unsure if this is hard minimum but makes logic safe for now, and
-    // corresponds to >= 4 knot intervals which doesn't seem overly restrictive
+  // returns matrix Rinf Eq(26) for size MxM, where M = mm
+  def matrixRinf(mm: Int): Array[Array[Double]] = {
+    // unsure if this is hard minimum but makes logic safe for now
     require(mm >= 7)
     val d0 = 16.0 / 6.0
     val d1 = -9.0 / 6.0
@@ -63,23 +65,22 @@ object infra {
     rf ++ rm ++ rl
   }
 
-  def matrixR(m: Int): Array[Array[Double]] = {
-    val mm = m + 3
-    // From (26)
-    val r = matrixRinf(m)
-    // From (27), upper left corner only, delaying 1/6 factor
+  def matrixR(mm: Int): Array[Array[Double]] = {
+    // From Eq(26)
+    val r = matrixRinf(mm)
+    // From Eq(27), upper left corner only, delaying 1/6 factor
     val rm = Array(
       Array(14.0,  -6.0,   0.0),
       Array(-6.0,   8.0,  -3.0),
       Array( 0.0,  -3.0,   2.0)
     )
-    // From (28), lower right corner only
+    // From Eq(28), lower right corner only
     val rp = Array(
       Array( 2.0,  -3.0,   0.0),
       Array(-3.0,   8.0,  -6.0),
       Array( 0.0,  -6.0,   14.0)
     )
-    // From (25): subtract rm and rp
+    // From Eq(25): subtract rm and rp
     for {
       j <- 0 until 3;
       k <- 0 until 3
@@ -89,4 +90,38 @@ object infra {
     }
     r
   }
+
+  // basis B3(t) from Eq(3) and Table 1
+  // Here, t is treated as normalized to unit distance between knot points
+  def b3(t: Double) = t match {
+    case t if t < 0.0 => 0.0
+    case t if t < 1 => math.pow(1.0 - t, 3) / 6.0
+    case t if t < 2 => (4.0 - (6.0 * t * t) + (3.0 * math.pow(t, 3))) / 6.0
+    case t if t < 3 => (1.0 + (3.0 * t) + (3.0 * t * t) - (3.0 * math.pow(t, 3))) / 6.0
+    case t if t < 4 => math.pow(t, 3) / 6.0
+    case _ => 0.0
+  }
+
+  // Eq(11)
+  // tk is array of knot points, dimension mm, aka M
+  def vectorb(alpha: Double, tk: Array[Double])(t: Double): Array[Double] =
+    tk.map { ti => b3(alpha * (t - ti)) }
+
+  // tk is array of knot points, dimension mm, aka M
+  // note this returns B-transposed relative to Eq(12).
+  // u is vector of data "x" points; in same axis as knot points tk
+  def matrixBT(alpha: Double, tk: Array[Double])(u: Array[Double]): Array[Array[Double]] = {
+    val b = vectorb(alpha, tk)(_)
+    u.map(b)
+  }
+
+  import org.apache.commons.math3.linear.Array2DRowRealMatrix
+
+/*
+  // constructs G, g and r from Eq(14) and Eq(68)
+  def qpObjectiveMatrices() = {
+    val r = 0.0  // per Eq(68)
+    (r)
+  }
+*/
 }
