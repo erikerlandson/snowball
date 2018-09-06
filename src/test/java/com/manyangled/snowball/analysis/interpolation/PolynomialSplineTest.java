5ea6c6ce7bc50d0dcb7ad5d2c7064e1b99bca38a
Erik Erlandson <eerlands@redhat.com>
2018-09-06 16:56:52 -0700

    PolynomialSplineTest skeleton

---
+++ src/test/java/com/manyangled/snowball/analysis/interpolation/PolynomialSplineTest.java
@@ -0,0 +1,14 @@
+package com.manyangled.snowball.analysis.interpolation;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertArrayEquals;
+
+import org.apache.commons.math3.analysis.polynomials.PolynomialSplineFunction;
+import org.apache.commons.math3.analysis.polynomials.PolynomialFunction;
+
+public class PolynomialSplineTest {
+    @Test
+    public void testSkeleton() {
+    }
+}
