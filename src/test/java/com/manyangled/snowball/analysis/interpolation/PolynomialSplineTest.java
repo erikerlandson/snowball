d4e6b9b1f98a2381f1bce8f9f3749e97725ad51e
Erik Erlandson <eerlands@redhat.com>
2018-09-07 15:42:30 -0700

    test standard-form polynomial construction

--- src/test/java/com/manyangled/snowball/analysis/interpolation/PolynomialSplineTest.java
+++ src/test/java/com/manyangled/snowball/analysis/interpolation/PolynomialSplineTest.java
@@ -7,8 +7,54 @@ import static org.junit.Assert.assertArrayEquals;
 import org.apache.commons.math3.analysis.polynomials.PolynomialSplineFunction;
 import org.apache.commons.math3.analysis.polynomials.PolynomialFunction;
 
+import static com.manyangled.snowball.analysis.interpolation.MSISupport.*;
+
 public class PolynomialSplineTest {
+    public static double refSpline(double x, double alpha, double[] tau, double[] K) {
+        assert tau.length == K.length;
+        int m = tau.length;
+        double s = 0.0;
+        for (int j = 0; j < m; ++j) {
+            s += tau[j] * B3(alpha * (x - K[j]));
+        }
+        return s;
+    }
+
     @Test
-    public void testSkeleton() {
+    public void test1() {
+        double[] tau = { 1.0, 2.0, 3.0, 4.0 };
+        int m = tau.length - 3;
+        double xmin = 10;
+        double xmax = 20;
+        double alpha = (double)m / (xmax - xmin);
+        double[] K = new double[tau.length];
+        for (int j = -3; j < m; ++j) {
+            K[j + 3] = xmin + ((double)j / alpha);
+        }
+        PolynomialSplineFunction spline = polynomialSplineFunction(tau, alpha, xmin);
+        for (double x = xmin; x <= xmax; x += (xmax - xmin)/100.0) {
+            double y = spline.value(x);
+            double yRef = refSpline(x, alpha, tau, K);
+            assertEquals(y, yRef, 1e-9);
+        }
+    }
+
+    @Test
+    public void test2() {
+        double[] tau = { 1.0, 2.0, 4.0, 8.0, 16.0, 32.0 };
+        int m = tau.length - 3;
+        double xmin = -10;
+        double xmax = 10;
+        double alpha = (double)m / (xmax - xmin);
+        double[] K = new double[tau.length];
+        for (int j = -3; j < m; ++j) {
+            K[j + 3] = xmin + ((double)j / alpha);
+        }
+        PolynomialSplineFunction spline = polynomialSplineFunction(tau, alpha, xmin);
+        for (double x = xmin; x <= xmax; x += (xmax - xmin)/1000.0) {
+            double y = spline.value(x);
+            double yRef = refSpline(x, alpha, tau, K);
+            assertEquals(y, yRef, 1e-9);
+        }
     }
 }
